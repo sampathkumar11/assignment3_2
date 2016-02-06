1c09a35dab6b3c0b3b7e2f49fe70bad4828ffce5
sampathkumar11 <sampathkumar11@gmail.com>
2016-02-06 09:51:57 +0530

    Assignment 3_2
    Errors present. Not completed

---
+++ src/assignment3_2/ChkPrime.java
@@ -0,0 +1,20 @@
+package assignment3_2;
+//finding a given number prime or not
+
+public class ChkPrime {
+//int n;
+
+void pc(c){
+	//System.out.println("Hello World");
+	//logic to check whether c is a prime no
+	String chk_result;
+	return chk_result;
+}
+	
+	public static void main(String args[]){
+		String c="10";// Given number
+		String out=pc(c);	
+		System.out.println("Given Number "+c+"is "+out);
+		
+	}
+}
