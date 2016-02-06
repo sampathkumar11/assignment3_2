aab3cdc27b16eb322ba13eb0080e3daf016636c4
sampathkumar11 <sampathkumar11@gmail.com>
2016-02-06 16:10:02 +0530

    Session 3-2
    Finished.

--- src/assignment3_2/ChkPrime.java
+++ src/assignment3_2/ChkPrime.java
@@ -1,20 +1,44 @@
 package assignment3_2;
 //finding a given number prime or not
-
+import java.util.Scanner;
 public class ChkPrime {
-//int n;
-
-void pc(c){
+	static String pc(String c){
 	//System.out.println("Hello World");
+		String chk_result;//="not run";
+		Float i=Float.parseFloat(c);
 	//logic to check whether c is a prime no
-	String chk_result;
-	return chk_result;
-}
+		int cnt=0;
+		if(i%2!=0){cnt++;}
+		if(i%3!=0){cnt++;}
+		if(i%5!=0){cnt++;}
+		if(i%6!=0){cnt++;}
+		if(i%7!=0){cnt++;}
+		if((i==2)||(i==3)||(i==5)||(i==7)){cnt++;}
+		if(cnt>4){
+			//System.out.println(i);
+			chk_result="Prime number.";
+		}else{
+			chk_result="Not a Prime number.";
+		}
+		return chk_result;
+	}
 	
 	public static void main(String args[]){
-		String c="10";// Given number
+		Scanner scnr=new Scanner(System.in);
+		System.out.println("Enter the number which has to be checked for Prime :");
+		String c = scnr.nextLine();
+		//String c="10";// Given number
 		String out=pc(c);	
-		System.out.println("Given Number "+c+"is "+out);
-		
+		System.out.println("Given Number "+c+" is "+out);
+		System.out.println("You want to try again ?\n (Enter y for Yes and n for No)");
+		String retry=scnr.nextLine();
+		if((retry=="y")||(retry=="Y")){
+			System.out.println("Enter the number which has to be checked for Prime :");
+			c = scnr.nextLine();
+			out=pc(c);	
+			}else{
+				System.out.println("Thanks for using. "+retry);
+			}
+		scnr.close();
+		}
 	}
-}
